6aa939a1419cc6a2fce3c4e0624397b21fa2ba5b
beejsbj <burooj.bj@gmail.com>
2024-01-31 18:18:46 -0500

    Modify the transformer
    merged core and semantic objects
    
    fixed color -> colors

--- sd.config.js
+++ sd.config.js
@@ -1,8 +1,23 @@
 const StyleDictionaryModule = require("style-dictionary");
 const { makeSdTailwindConfig } = require("sd-tailwindcss-transformer");
 
+const fs = require("fs");
+
+const tokens = require("./tokens/converted-tokens.json");
+const _ = require("lodash");
+const finalTokens = _.merge(tokens.ds.core, tokens.ds.semantic);
+finalTokens.colors = finalTokens.color;
+delete finalTokens.color;
+
+fs.writeFileSync(
+  "./tokens/tailwind-converted-tokens.json",
+  JSON.stringify(finalTokens)
+);
+
 const StyleDictionary = StyleDictionaryModule.extend(
   makeSdTailwindConfig({
+    source: ["tokens/tailwind-converted-tokens.json"],
+    buildPath: "build/tailwind/",
     type: "all",
     tailwind: {
       plugins: ["forms", "typography"],
